03184eb6a7d3dd3be44cec56e56c2c48f731c1a1
unknown <rsanders@R0706932.campus.ltu.edu>
2014-01-27 18:58:18 -0500

    Added scripts for GUI control to game menus

---
+++ Assets/Scripts/RS_GUIControl_Credits.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RS_GUIControl_Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Handles GUI functions
+	void OnGUI () {
+		GUI.Box (new Rect (Screen.width / 8, Screen.height / 8, 3 * Screen.width / 4, 3 * Screen.height / 4), "Credits");
+	}
+}
---
+++ Assets/Scripts/RS_GUIControl_Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RS_GUIControl_Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//Handles GUI functions
+	void OnGUI () {
+
+		//code for main menu
+		if (GUI.Button (new Rect(3 * Screen.width / 8, Screen.height / 2, Screen.width / 4, 50), "Play"))
+			Application.LoadLevel("MainGameplay");
+		
+		if (GUI.Button (new Rect(3 * Screen.width / 8, Screen.height / 2 + 70, Screen.width / 4, 50), "Options"))
+				Application.LoadLevel("Options");
+		
+		if (GUI.Button (new Rect(3 * Screen.width / 8, Screen.height / 2 + 140, Screen.width / 4, 50), "Credits"))
+			Application.LoadLevel("Credits");
+
+	}
+
+}
