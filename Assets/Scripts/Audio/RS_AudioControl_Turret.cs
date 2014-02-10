fcf24d031328c498ceff27aaa73bacff86305d02
unknown <rsanders@R0706932.campus.ltu.edu>
2014-02-10 18:32:19 -0500

    Audio scripts (in progress)

---
+++ Assets/Scripts/Audio/RS_AudioControl_Turret.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RS_AudioControl_Turret : MonoBehaviour {
+
+	/*
+	 * Begin:
+	 * Ensures that a duplicate object playing this sound doesn't exist.
+	 * */
+	private static RS_AudioControl_Turret instance = null;
+	
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	/*
+	 * End
+	 * */
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
