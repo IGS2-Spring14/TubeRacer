8eaad3c4621e406e3090f5209bfebe01cea0f04a
RoyaleLetourneau <rletourne@ltu.edu>
2014-02-11 16:22:22 -0800

    Updated TurretController, removed reverse aim (haven't tested in scene yet)

--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -5,7 +5,6 @@ public class TurretController : MonoBehaviour
 {
 	//Aiming
 	public float MaxGunAngle = 60f;
-	public bool ReverseAim = true;
 	public Transform target, GunTransform;
 
 	//Firing
@@ -40,8 +39,6 @@ public class TurretController : MonoBehaviour
 		//Direction to look at (needs to be reversed so model faces player)
 		//Vector3 relPos = target.position - transform.position;
 		relPos = Spline.GetPointAtTime (Time.time + 3) - transform.position;
-		if (ReverseAim)
-			relPos = relPos * -1;
 		relPos.y = 0.0f;
 
 		//Face the turret toward the player (y is axis of rotation)
@@ -50,8 +47,6 @@ public class TurretController : MonoBehaviour
 		//Face the gun toward the player
 		//relPos = target.position - transform.position;
 		relPos = Spline.GetPointAtTime (Time.time + 3) - transform.position;
-		if (ReverseAim)
-			relPos = relPos * -1;
 		if (Vector3.Angle(relPos, transform.forward) <= MaxGunAngle)
 			GunTransform.rotation = Quaternion.LookRotation(relPos);
 
