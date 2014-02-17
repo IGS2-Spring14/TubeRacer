b936771aa9b9e34b1a553882e19d7ff0105ee6ec
unknown <rsanders@R0706932.campus.ltu.edu>
2014-02-17 17:26:12 -0500

    Added sound for player ship engine

---
+++ Assets/Scripts/Audio/RS_AudioControl_Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RS_AudioControl_Player : MonoBehaviour {
+
+	public AudioSource bgm;
+	private bool destroySelf = false;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this.gameObject);
+
+		//tells self not to destroy self
+		//destroySelf = false;
+
+		//plays audio clip tied to AudioSource if not currently playing
+		if (!bgm.isPlaying)
+			bgm.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//If set to destroy (destroyAudio = true), stops the sound and destroys self
+		if (destroySelf){
+			bgm.Stop();
+			Destroy(this.gameObject);
+		}
+	}
+
+	/*
+	 * RemoveObject
+	 * Function: Allows for other objects to destroy this object
+	 * */
+	public void RemoveObject(){
+		destroySelf = true;
+	}
+}
