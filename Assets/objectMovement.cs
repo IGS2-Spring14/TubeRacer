3f821533ad53902a91f3b765a8214bae678d5d68
Joel MacFadyen <jamage.art@gmail.com>
2014-02-26 15:49:02 -0500

    Added "Joel" scene with "objectMovement" script that allows me to rotate and have objects move in the scene. Changed horseshoe spline so that the final part is rotated.

---
+++ Assets/objectMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectMovement : MonoBehaviour {
+
+	public float pipeRotationX = 10;
+	public float pipeRotationY = 10;
+	public float pipeRotationZ = 10;
+
+	public float movSpeedX = 100;
+	public float movSpeedY = 100;
+	public float movSpeedZ = 100;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//rotate object on X axis pipeRotationX degrees per second
+		transform.Rotate (Vector3.right * pipeRotationX * Time.deltaTime);
+		//rotate object on Y axis pipeRotationY degrees per second
+		transform.Rotate (Vector3.up * pipeRotationY * Time.deltaTime);
+		//rotate object on Z axis pipeRotationZ degrees per second
+		transform.Rotate (Vector3.forward * pipeRotationZ * Time.deltaTime);
+
+		//move object movSpeed distance per second
+		transform.position += new Vector3(movSpeedX * Time.deltaTime, movSpeedY * Time.deltaTime, movSpeedZ * Time.deltaTime);
+
+	}
+}
