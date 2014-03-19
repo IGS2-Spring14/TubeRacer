7e4fc072a30db97ee058d5f74c524dff03cfd5dd
diogobernini <diogo@diogobernini.com>
2014-03-19 15:35:47 -0400

    Life script added.

---
+++ Assets/Scripts/Life_Default.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life_Default : MonoBehaviour {
+	public int life;
+	GameObject clone;
+		// Use this for initialization
+	void Start () {
+		clone = this.gameObject;
+		life = 3;
+	}
+
+	public void Die()
+	{
+		Debug.Log(this.name.ToString() + " died");
+		Destroy (clone);
+		//insert here the activity that will happen when the object dies
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (life == 0) {
+			Die();
+				}
+	}
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.CompareTag ("Gun")) {
+						Debug.Log (this.name.ToString () + " was hit");
+						life--;
+				}
+	}
+}
