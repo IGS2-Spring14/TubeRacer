68b43fdbadf50e2080faa899b131be6d11586bba
RoyaleLetourneau <rletourne@ltu.edu>
2014-03-21 18:23:53 -0700

    I created a custom inspector for moving obstacles. Only the MovingPattern is complete so far for them, but more updates to come.

---
+++ Assets/Editor/ObstacleBuilderEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ObstacleContoller))]
+public class ObstacleBuilderEditor : Editor
+{
+    private string[] obstacleTypeNames = { "MovingPattern", "SuddenMovement", "RotatingPattern" };
+    private int obstacleSelectionIndex = 0;
+
+    public override void OnInspectorGUI()
+    {
+        GUILayout.Label("Obstacle Builder: choose obstacle type... (all pending further updates)");
+
+        obstacleSelectionIndex = GUILayout.Toolbar(obstacleSelectionIndex, obstacleTypeNames);
+        GUILayout.Space(10f);
+        ObstacleContoller obstacle = (ObstacleContoller)target;
+
+        if (obstacleSelectionIndex == 0)
+        {
+            obstacle.obstacleType = ObstacleContoller.ObstacleType.MovingPattern;
+
+            GUILayout.Label("MovingPattern obstacle will move in the selected direction first,\nthen move the opposite direction (repeating)");
+            GUILayout.Space(10f);
+
+            obstacle.direction = (ObstacleContoller.Direction)EditorGUILayout.EnumPopup("Direction", obstacle.direction);
+            obstacle.TraverseDistance = EditorGUILayout.FloatField("Traverse Distance", obstacle.TraverseDistance);
+            obstacle.speed = EditorGUILayout.FloatField("Speed", obstacle.speed);
+        }
+        else if (obstacleSelectionIndex == 1)
+        {
+            obstacle.obstacleType = ObstacleContoller.ObstacleType.SuddenMovement;
+
+            GUILayout.Label("Not yet implemented");
+            GUILayout.Label("SuddenMovement obstacle will hold its position until the player\nenters its trigger box to move in a direction (not repeating)");
+            GUILayout.Space(10f);
+        }
+        else if (obstacleSelectionIndex == 2)
+        {
+            obstacle.obstacleType = ObstacleContoller.ObstacleType.RotatingPattern;
+
+            GUILayout.Label("Not yet implemented");
+            GUILayout.Space(10f);
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleContoller.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ObstacleContoller : MonoBehaviour 
+{
+    public enum ObstacleType { MovingPattern, SuddenMovement, RotatingPattern };
+    public ObstacleType obstacleType = ObstacleType.MovingPattern;
+
+    public enum Direction { UP, DOWN, LEFT, RIGHT, FORWARD, BACKWARD };
+    public Direction direction = Direction.DOWN;
+    public Vector3 Velocity = Vector3.zero, prevPosition;
+    public float TraverseDistance = 3000;
+    private float traverseDistance = 0.0f;
+    public float speed = 30f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        prevPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        traverseDistance += Vector3.Distance(transform.position, prevPosition);
+        prevPosition = transform.position;
+
+        if (obstacleType == ObstacleType.MovingPattern)
+        {
+            UpdateMovingPattern();
+        }
+	}
+
+    private void UpdateMovingPattern()
+    {
+        if (direction == Direction.DOWN)
+            Velocity = new Vector3(0, -1, 0);
+        else if (direction == Direction.UP)
+            Velocity = new Vector3(0, 1, 0);
+        else if (direction == Direction.LEFT)
+            Velocity = new Vector3(-1, 0, 0);
+        else if (direction == Direction.RIGHT)
+            Velocity = new Vector3(1, 0, 0);
+        else if (direction == Direction.FORWARD)
+            Velocity = new Vector3(0, 0, -1);
+        else
+            Velocity = new Vector3(0, 0, 1);
+
+        if (traverseDistance < TraverseDistance)
+            transform.Translate(Velocity * speed);
+        else
+        {
+            traverseDistance = 0.0f;
+            if (direction == Direction.DOWN || direction == Direction.UP)
+                direction = (direction == Direction.DOWN) ? Direction.UP : Direction.DOWN;
+            else if (direction == Direction.LEFT || direction == Direction.RIGHT)
+                direction = (direction == Direction.LEFT) ? Direction.RIGHT : Direction.LEFT;
+            else
+                direction = (direction == Direction.FORWARD) ? Direction.BACKWARD : Direction.FORWARD;
+        }
+    }
+}
